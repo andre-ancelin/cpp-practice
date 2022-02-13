bfaa1f7d3ee096a9af048f78ad211c375aa601ae
Andre Ancelin <andre.ancelin@adtecdigital.net>
2022-02-13 17:52:18 -0500

    Exploring virtual functions

--- cpp-practice.cpp
+++ cpp-practice.cpp
@@ -1,8 +1,58 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char **argv) {
-	cout << "Hello world";
-	cout << "Development branch in GIT";
+typedef enum Note {A, B, C, D, E, F, G} Note;
+
+class Instrument {
+public:
+	virtual void playNote (const Note n) const = 0;
+};
+
+class Trumpet : public Instrument {
+public:
+	virtual void playNote (const Note n) const {
+		cout << "Trumpet " << n << endl;
+	}
+};
+
+class Piano : public Instrument {
+public:
+	virtual void playNote (const Note n) const {
+		cout << "Piano " << n << endl;
+	}
+};
+
+////static void
+////tune (
+////	Instrument 	&instrument,
+////	const Note	note
+////){
+////	instrument.playNote(note);
+////}
+
+
+int
+main (
+	int		argc,
+	char	**argv
+){
+	//cout << "Hello world" << endl;
+	//cout << "Development branch in GIT" << endl;
+
+	Trumpet trumpet;
+	Piano piano;
+
+	//tune(trumpet, A);
+	//tune(piano, C);
+
+//	Instrument *
+//	i = &trumpet;
+//	i->playNote(D);
+	((Instrument&)trumpet).playNote(A);
+	trumpet.playNote(B);
+
+	((Instrument&)piano).playNote(C);
+	piano.playNote(D);
+
 	return 0;
 }
